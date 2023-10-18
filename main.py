54f43bbc1f3e4c22f638edfe29fe039cefe1cc28
ngchongin <ngchongin@gmail.com>
2023-10-17 23:24:20 -0400

    update main.py

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ app = Flask(__name__)
 def home():
     return 'Hello, World!'
 
+def run_websocket():
+    websocket_server = WebSocketServer('0.0.0.0', 8000)
+    print("22222222222222222")
+    asyncio.run(websocket_server.run())
 
 if __name__ == '__main__':
     # start job manager to handle the game flow
@@ -29,15 +33,17 @@ if __name__ == '__main__':
     SystemConfig.instance().set_tables()
 
     print("000000000000000000")
+     # start the web server to handle cient connection for pushing message to client
+    
+    thread = threading.Thread(target=run_websocket)
+    thread.start()
+
+    print("3333333333333")
+
     # start web server to handle client request for client easily to handle timeout
     # or maybe later can accept other service api call.
     http_server = HttpServer(host='0.0.0.0', port=8080)
-    thread = threading.Thread(target=app.run)
-    thread.start()
+    app.run()
     print("111111111111111111")
-    # start the web server to handle cient connection for pushing message to client
-    websocket_server = WebSocketServer('0.0.0.0', 8000)
-    print("22222222222222222")
-    asyncio.run(websocket_server.run())
-    print("3333333333333")
+   
 
